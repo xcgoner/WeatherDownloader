2ac44bf9526f26f55d3f2f05aa6f6d130c6d614f
xcgoner <xcgoner1108@gmail.com>
2015-12-17 17:02:36 +0800

    rename for ru5

--- src/WeatherDownloader/Downloader_ru5.java
+++ src/WeatherDownloader/Downloader_ru5.java
@@ -23,7 +23,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class Downloader {
+public class Downloader_ru5 {
 	
 	static private HttpResponse httpResponse;
 	static private DefaultHttpClient httpClient = new DefaultHttpClient();
